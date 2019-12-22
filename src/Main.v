e89d108785997967a5aa6e857443f54e19f4a59a
Guillaume Claret <dev@clarus.me>
2019-12-22 17:13:18 +0100

    Print the base of the bonsai

--- src/Main.v
+++ src/Main.v
@@ -1,4 +1,5 @@
 Require Import Coq.Lists.List.
+Require Import Coq.Strings.Ascii.
 Require Import Coq.Strings.String.
 Require Import Io.All.
 Require Import Io.System.All.
@@ -7,8 +8,47 @@ Require Import ListString.All.
 Import ListNotations.
 Import C.Notations.
 
+Local Open Scope string.
+Local Open Scope list.
+
+Notation "x ^^ y" := (String.append x y) (right associativity, at level 60).
+
+Definition columns := 50.
+Definition rows := 50.
+
+Module Colors.
+  Definition escape : string -> string := String (ascii_of_nat 27).
+  Definition LightBrown := escape "[38;5;172m".
+  Definition DarkBrown := escape "[38;5;130m".
+  Definition BrownGreen := escape "[38;5;142m".
+  Definition Green := escape "[38;5;106m".
+  Definition Gray := escape "[38;5;243m".
+  Definition R := escape "[0m".
+End Colors.
+
+Module Base.
+  Definition width := 15.
+  Definition art_lines : list string := [
+    Colors.Gray ^^ ":" ^^ Colors.Green ^^ "___________" ^^ Colors.DarkBrown ^^
+    "./~~\." ^^ Colors.Green ^^ "___________" ^^ Colors.Gray ^^ ":";
+    " \                          /";
+    "  \________________________/";
+    "  (_)                    (_)"
+  ].
+  Definition art_lines_with_shift : list LString.t :=
+    List.map (fun art_line =>
+      LString.repeat (LString.s " ") (Nat.div2 columns - width) ++
+      LString.s art_line
+    ) art_lines.
+End Base.
+
 Definition main (argv : list LString.t) : C.t System.effect unit :=
-  System.log (LString.s "Bonsai!").
+  let new_line := "
+" in
+  System.log (
+    LString.join (LString.s new_line) Base.art_lines_with_shift ++
+    LString.s Colors.R
+  ).
 
 (** Extract the Bonsai program to `extraction/main.ml`. Run the `Makefile`
     in `extraction/` to compile it. *)
