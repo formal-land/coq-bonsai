8bfbb3c3a8323ecd4e2a7fc327570ed4d13461a7
Guillaume Claret <dev@clarus.me>
2019-12-23 15:44:26 +0100

    Update the license to GPL3 as it is heavily inspired by a GPL3 program

--- src/Main.v
+++ src/Main.v
@@ -1,3 +1,23 @@
+(*
+  Bonsai generator for the terminal
+
+  Copyright (C) 2019 Guillaume Claret
+  Copyright (C) 2019 John Allbritten
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*)
+
 Require Import Coq.Lists.List.
 Require Import Coq.Strings.Ascii.
 Require Import Coq.Strings.String.
